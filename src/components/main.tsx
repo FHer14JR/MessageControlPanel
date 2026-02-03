21d4544c9475f236cee7527261a5a2e7404c41bc
FHer14JR <freddy.herrera9998@gmail.com>
2026-02-03 16:31:17 -0600

    Update

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AdminDashboard from './AdminDashboard';
-import PublicRequestForm from './componets/PublicRequestForm';
-import './index.css';
-
-// Esta función decide qué mostrar según la URL
-const App = () => {
-  const isAdmin = window.location.pathname === '/admin';
-  return isAdmin ? <AdminDashboard /> : <PublicRequestForm />;
-};
+import AdminDashboard from '../AdminDashboard'; 
+import '../index.css'; 
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('No se encontró el elemento root');
 
-createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <AdminDashboard />
+    </React.StrictMode>
+  );
+}
